d62f6b8b925850c48ffb3bcff51862cfad5ead0a
Bochan <himanshubora972@gmail.com>
2025-06-02 20:39:07 +0530

    day5b

---
+++ mergesort.java
@@ -0,0 +1,59 @@
+
+//merge sort
+import java.util.ArrayList;
+
+public class mergesort {
+    public static void main(String[] args) {
+        int[] arr = { 2, 7, 1, 5, 6 };
+        System.out.println("Before sorting array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        mergesort(arr, 0, arr.length - 1);
+        System.out.println("After sorting array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void mergesort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int mid = (high + low) / 2;
+        mergesort(arr, low, mid);
+        mergesort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+
+    public static void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+            while (left <= mid) {
+                temp.add(arr[left]);
+                left++;
+
+            }
+        
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+
+    }
+}
