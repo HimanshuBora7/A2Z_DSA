9ebda71d7d794b071fef98c11e4ace0610340e10
Bochan <himanshubora972@gmail.com>
2025-06-18 16:24:55 +0530

    optimal approach to find the intersection of two arrays

--- intersection.java
+++ intersection.java
@@ -12,48 +12,82 @@ import java.util.Arrays;
 //-----  BRUTE -- FORCE -- APPROACH -----
 
 public class intersection {
-    public static void main (String [] args ){
-        int [] arr1 = {1,2,2,3,3,4,5,6};
-        int [] arr2 = {2,3,3,5,6,6,7};
+    public static void main(String[] args) {
+        int[] arr1 = { 1, 2, 2, 3, 3, 4, 5, 6 };
+        int[] arr2 = { 2, 3, 3, 5, 6, 6, 7 };
         System.out.println("ARRRAY 1 : ");
-        for(int x : arr1){
+        for (int x : arr1) {
             System.out.print(x + " ");
         }
         System.out.println("\nArray 2 : ");
-        for(int y : arr2 ){
+        for (int y : arr2) {
             System.out.print(y + " ");
         }
-        ArrayList<Integer> newArray = intersect(arr1, arr2);
-        System.out.println("\nFinal Intersected array");
-        for(int z : newArray){
+        // ArrayList<Integer> newArray = intersect(arr1, arr2);
+        // System.out.println("\nFinal Intersected array");
+        // for (int z : newArray) {
+        //     System.out.print(z + " ");
+        // }
+        ArrayList<Integer> newArray2 = intersect_optimal(arr1, arr2);
+        System.out.println("\nFinal Intersected array2");
+        for (int z : newArray2) {
             System.out.print(z + " ");
         }
 
     }
-    public static ArrayList<Integer> intersect(int arr1 [] ,int arr2 []){
-    int n1 = arr1.length;
-    int n2 = arr2.length;
-    
-    //Array list to store the result 
 
-    ArrayList<Integer> ans = new ArrayList<>();
+    // --brute force approach --
+    public static ArrayList<Integer> intersect(int arr1[], int arr2[]) {
+        int n1 = arr1.length;
+        int n2 = arr2.length;
 
-    int [] visted = new int[n2]; // to keep check if the element from 2nd array is already visited or not 
-    Arrays.fill(visted,0);
-    for(int i = 0 ; i <n1 ;i++){
-        for(int j = 0 ; j < n2 ; j++){
-            if(arr1[i] == arr2[j] && visted[j] ==0){
-             ans.add(arr1[i]);
-             visted[j] = 1;
-             break;
+        // Array list to store the result
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int[] visted = new int[n2]; // to keep check if the element from 2nd array is already visited or not
+        Arrays.fill(visted, 0);
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                if (arr1[i] == arr2[j] && visted[j] == 0) {
+                    ans.add(arr1[i]);
+                    visted[j] = 1;
+                    break;
+                }
+                if (arr2[j] > arr1[i]) {
+                    break;
+                }
+
             }
-            if(arr2[j] > arr1[i]){
-                break;
-            }
+        }
+        return ans;
+    }
+
+    // --optimal approach //
+    public static ArrayList<Integer> intersect_optimal(int[] arr1, int[] arr2) {
+
+        int n1 = arr1.length;
+      
+        ArrayList<Integer> arr3 = new ArrayList<>();
+
+        
+        int i=0,j =0;
+
+        while(i < arr1.length && j < arr2.length){
+         if(arr1[i] == arr2[j]){
+            arr3.add(arr1[i]);
+            i++;
+            j++;
 
         }
+        else if( arr1[i] < arr2[j]){
+            i++;
+        }
+        else{
+            j++;
+        }
+
     }
-    return ans;
-    }
-    
+return arr3;
+}
 }
