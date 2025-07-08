0c696ed038b5b644b14ae4ec3371d1622e5b9be6
himanshu <himanshubora972@gmail.com>
2025-07-08 17:56:33 +0530

    another efficient approach to solve the next permutation problem

--- next_permutation.java
+++ next_permutation.java
@@ -20,21 +20,22 @@ public class next_permutation {
             System.out.print(x + " ");
         }
 
-        int[] arr2 = optimal_approach(arr);
+        // int[] arr2 = optimal_approach_mine(arr);
+        optimal_approach_right(arr);
 
         System.out.println("next permutation array ");
-        for (int x : arr2) {
+        for (int x : arr) {
             System.out.print(x + " ");
         }
 
     }
 
-    public static int[] optimal_approach(int[] arr) {
+    public static int[] optimal_approach_mine(int[] arr) {
 
         int a = -1;
         int b = -1;
 
-        for (int i = 0; i < arr.length-1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
 
             if (arr[i] < arr[i + 1]) {
                 a = i;
@@ -44,7 +45,6 @@ public class next_permutation {
         // System.out.println("a and b are : " + arr[a] + " , " + arr[b]);
 
         int diff = Integer.MAX_VALUE;
-       
 
         for (int j = b; j < arr.length; j++) {
             if (arr[a] < arr[j]) {
@@ -65,9 +65,8 @@ public class next_permutation {
 
         // System.out.println("intermediate array ");
         // for(int x : arr){
-        //    System.out.print(" " + x );
+        // System.out.print(" " + x );
         // }
-
 
         for (int i = b; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
@@ -79,5 +78,68 @@ public class next_permutation {
             }
         }
         return arr;
+    }
+
+    // in this approach we try to solve the problem from behind as we know they wld
+    // be in increasing order from las
+    // so we fist find the breakpoint or dip which possible last can have at n-2th
+    // to index to 0th index
+    // we store the index at index variable which would be initialised as -1
+    // initially if after tbe 1st step if it is still -1
+    // then we can say that we have reached the end of the order so we have to
+    // return the 1st permutaion order which can be
+    // made easily by reversing the given array , in this way we have handeled the
+    // edge case
+    // second step if the index is not -1 , we find the just big number to swap with
+    // the element at the breakpoint
+    // so we start looking from n-1 to the index and swap with the first element we
+    // get greater than that
+    // 3 rd and final step after swapping would be arranging the remaining element
+    // in increasing order from index +1 to arr.length
+    public static void optimal_approach_right(int[] arr) {
+
+        int index = -1;
+        int n = arr.length;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                index = i;
+            }
+        }
+        // System.out.println("\ngot breakpoint at "+ index + " element : " +
+        // arr[index]);
+        if (index == -1) { // it means we have reached the last order of the permuatation at this point to
+                           // get the 1st order we just to have to reverse the array
+            int z = arr[n - 1];
+
+            for (int i = 0; i < n - 1; i++) {
+                arr[i + 1] = arr[i];
+            }
+            arr[0] = z;
+        }
+
+        else {
+            // else means we have got the index of breakpoint now frm behind we to select
+            // the just larger number
+            for (int i = n - 1; i > index; i--) {
+                if (arr[i] > arr[index]) {
+                    int temp = arr[index];
+                    arr[index] = arr[i];
+                    arr[i] = temp;
+                    break; // we want the first element vch whld be just grea8er frm last so use break
+                }
+            }
+
+            // after swappin we have to sort the remaining element
+            for (int j = index + 1; j < n - 1; j++) {
+                for (int f = j + 1; f < n; f++) {
+                    if (arr[j] > arr[f]) {
+                        int temp = arr[f];
+                        arr[f] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
     }
 }
