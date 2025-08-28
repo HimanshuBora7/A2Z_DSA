10e5ad0db3824dd1064a3468c040f8ad0a550d8a
himanshubora7 <himanshubora972@gmail.com>
2025-08-28 08:35:45 +0530

    binary search ~ recursive approach

--- Binary_search_1D/binary_search.java
+++ Binary_search_1D/binary_search.java
@@ -1,5 +1,32 @@
 package Binary_search_1D;
 
+// recursive approach 
 public class binary_search {
+    public static void main(String[] args) {
+        int arr[] = { 3, 4, 6, 7, 9, 12, 16, 17 };
+        int n = 9;
+        int output = recur_approach(arr, 0, arr.length, n);
+        if (output == -1) {
+            System.out.println("Not found");
+        } else {
+            System.out.println("Found at " + (output + 1));
+        }
 
+    }
+
+    public static int recur_approach(int[] arr, int low, int high, int target) {
+        if (low >= high) {
+            return -1;
+        }
+
+        int mid = (low + high) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] > target) {
+            return recur_approach(arr, low, mid - 1, target);
+        } else {
+            return recur_approach(arr, mid + 1, high, target);
+        }
+
+    }
 }
