123591146b702f2750b8d541fd2d199834fe40e6
himanshubora7 <himanshubora972@gmail.com>
2026-02-23 19:25:29 +0530

    book allocation ~ optimal approach

--- Binary_search_1D/MinimumNumberofPages.java
+++ Binary_search_1D/MinimumNumberofPages.java
@@ -9,7 +9,8 @@ public class MinimumNumberofPages {
         int arr[] = { 25, 46, 28, 49, 24 };
         int m = 4;
 
-        int ans = brute(arr, m);
+        // int ans = brute(arr, m);
+        int ans = optimal(arr, m);
         System.out.println("minimum maximum pages allocated  " + ans);
     }
 
@@ -19,6 +20,11 @@ public class MinimumNumberofPages {
     // till where the ranage goes ? when we allocate every book to single person
     // which is summation of every element in the array
     public static int brute(int[] arr, int m) {
+
+        // answer not possible in this scnerio
+        if (m > arr.length)
+            return -1;
+
         // finding low and high
 
         int maxElem = Integer.MIN_VALUE;
@@ -58,4 +64,44 @@ public class MinimumNumberofPages {
         }
         return student;
     }
+
+    // optimising this is simple we have range of answers we have to find the
+    // minimum number of maximum pages pages
+    // for any given specific page mid if we get the answer we trim down the right
+    // search space to see if we can minimixe the pages allocation
+    // if we don;t get the answer we trim down the left search space and try to
+    // increase the count of page and find the answer
+
+    public static int optimal(int[] arr, int m) {
+        if (m > arr.length)
+            return -1;
+
+        int maxElem = Integer.MIN_VALUE;
+        int sumElem = 0;
+        for (int x : arr) {
+            if (x > maxElem) {
+                maxElem = x;
+            }
+            sumElem += x;
+        }
+
+        int low = maxElem;
+        int high = sumElem;
+        int minAns = Integer.MAX_VALUE;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int countofStudent = countStudents(arr, mid);
+            if (countofStudent <= m) {
+                if (mid < minAns) {
+                    minAns = mid;
+                    high = mid - 1;
+                }
+            } else {
+                low = mid + 1;
+            }
+        }
+        return minAns != Integer.MAX_VALUE ? minAns : -1;
+    }
+
 }
